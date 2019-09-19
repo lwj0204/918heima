40139d9fed68df55427e2ae44ceb70b5633af3af
lwj0204 <395091873@qq.com>
2019-09-19 15:36:51 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>我是主页</div>
+  <el-container>
+    <!-- 左边容器 -->
+    <el-aside>左边</el-aside>
+    <el-container>
+      <el-header></el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
