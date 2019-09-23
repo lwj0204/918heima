b399fa8a4f0b02ecca3f9b2b9bc5a6610ca084a9
lwj0204 <395091873@qq.com>
2019-09-23 21:44:19 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,11 +5,15 @@
       </bread-crumb>
       <!-- 表格列表 -->
       <el-table :data="list">
-          <el-table-column width="600px" label="标题" prop="title"></el-table-column>
-          <el-table-column align="center" label="评论状态" prop="comment_status"></el-table-column>
+          <el-table-column width="500px" label="标题" prop="title"></el-table-column>
+          <el-table-column :formatter="stateFormatter" align="center" label="评论状态" prop="comment_status"></el-table-column>
           <el-table-column align="center" label="总评论数" prop="total_comment_count"></el-table-column>
           <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
+          <el-table-column label="操作">
+              <!-- 自定义内容 -->
+              <el-button type="text" size="small">修改</el-button>
+              <el-button type="text" size="small">关闭评论</el-button>
+          </el-table-column>
       </el-table>
   </el-card>
 </template>
@@ -30,6 +34,9 @@ export default {
       }).then(result => {
         this.list = result.data.results
       })
+    },
+    stateFormatter (row, column, cellvalue, index) {
+      return cellvalue ? '正常' : '关闭'
     }
   },
   created () {
