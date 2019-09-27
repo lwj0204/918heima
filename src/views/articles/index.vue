ebd2003bc63bdf1219fb2b63adc9360acd5b9e08
lwj0204 <395091873@qq.com>
2019-09-27 19:24:57 +0800

    黑马头条PC-内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,12 +3,40 @@
       <bread-crumb slot="header">
         <template slot="title">内容列表</template>
       </bread-crumb>
+      <el-form style="margin-left:50px">
+          <el-form-item label="文章状态：">
+              <el-radio-group>
+                  <el-radio>全部</el-radio>
+                  <el-radio>草稿</el-radio>
+                  <el-radio>待审核</el-radio>
+                  <el-radio>审核通过</el-radio>
+                  <el-radio>审核失败</el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道列表：">
+              <el-select>
+                  <el-option label=""></el-option>
+              </el-select>
+          </el-form-item>
+          <el-form-item label="时间选择：">
+                <el-date-picker
+                    v-model="value1"
+                    type="daterange"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
+          </el-form-item>
+      </el-form>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
 
+    }
+  }
 }
 </script>
 
